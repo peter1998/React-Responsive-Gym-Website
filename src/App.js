f952fce6e2319c9d11f0f98695a87b15e31076cb
Peter Matov <pmatov@gmail.com>
2022-11-18 18:23:42 +0200

    [C] Programs Component and styling.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/Hero/Programs/Programs";
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
     </div>
   );
 }
