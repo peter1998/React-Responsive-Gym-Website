88cc67836cb38b6a46b663bae9edf759eb71b1ba
Peter Matov <pmatov@gmail.com>
2022-11-21 18:45:18 +0200

    [C]  Reasons style and structure components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Programs from "./components/Hero/Programs/Programs";
+import Reasons from "./components/Hero/Reasons/Reasons";
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reasons />
     </div>
   );
 }
