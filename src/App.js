2a08abe804c55e0d0ef383a16be7c0355c5b28f9
Peter Matov <pmatov@gmail.com>
2022-11-24 22:52:27 +0200

    [C] Plan component implement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Plans from "./components/Hero/Plans/Plans";
 import Programs from "./components/Hero/Programs/Programs";
 import Reasons from "./components/Hero/Reasons/Reasons";
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Hero />
       <Programs />
       <Reasons />
+      <Plans />
     </div>
   );
 }
